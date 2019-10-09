3ff6ca2e9f2d7e8e4597593b1862c401052bea91
AlejandroLijoi <alejandrolijoi@live.com>
2019-10-09 13:27:58 -0300

    parte de la base /Table |agenda|history | pet | petType | ServiceType ----- ///// -------  Controller Agenda & owner

---
+++ Vet.Web/Migrations/20191008190636_Owner.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vet.Web.Migrations
+{
+    public partial class Owner : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owner",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Document = table.Column<string>(maxLength: 20, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    FixelPhone = table.Column<string>(maxLength: 20, nullable: true),
+                    CellPhone = table.Column<string>(maxLength: 20, nullable: false),
+                    Address = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owner", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owner");
+        }
+    }
+}
---
+++ Vet.Web/Migrations/20191008215005_FixedPhone.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vet.Web.Migrations
+{
+    public partial class FixedPhone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FixelPhone",
+                table: "Owner",
+                newName: "FixedPhone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FixedPhone",
+                table: "Owner",
+                newName: "FixelPhone");
+        }
+    }
+}
